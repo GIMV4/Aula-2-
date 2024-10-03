3cf7899d607650ccaf46878041ddfe0f5107d326
GIMV4 <133161689+GIMV4@users.noreply.github.com>
2024-10-03 11:59:07 -0300

    dsa

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let estados = ['AC','AL','AP','AM','BA','CE','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RF','RO','RR','SC','SP','SE','TO'];
 let i = 0
 while(i<27){
+    console.log(i);
     console.log(estado[i]);
     i=i+1;
 }
