703ca2e39fed37f985f8e53260a0481b131f9df5
GIMV4 <133161689+GIMV4@users.noreply.github.com>
2024-10-03 12:06:07 -0300

    dasdsa

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let estados = ['AC','AL','AP','AM','BA','CE','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RF','RO','RR','SC','SP','SE','TO','kalliani está copiando'];
+let estados = ['AC','AL','AP','AM','BA','CE','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RF','RO','RR','SC','SP','SE','TO'];
 let i = 0
 while(i<estados.length){
     console.log(i);
